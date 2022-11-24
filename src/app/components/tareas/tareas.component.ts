f18cf2be0fd52b49d3412bebf6ff33863a75a709
Mirkko16 <mirkko1996@hotmail.com>
2022-11-24 19:31:38 -0300

    aplicacion final

--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tarea } from 'src/app/models/tarea';
 
 @Component({
   selector: 'app-tareas',
   templateUrl: './tareas.component.html',
   styleUrls: ['./tareas.component.css']
 })
-export class TareasComponent {
+export class TareasComponent implements OnInit {
+  
+  listaDeTareas: Tarea[]=[];
+  nombreTarea:string='';
+
+  constructor(){ }
+
+  ngOnInit(): void {    
+  }
+
+  AgregarTarea(){
+      const tarea: Tarea = {
+      nombre: this.nombreTarea,
+      estado: false
+    }
+    
+    this.listaDeTareas.push(tarea);
+
+    this.nombreTarea ='';
+  }
+  eliminar(i:number){
+    this.listaDeTareas.splice(i,1);
+  }
+
+  actualizarTarea(tarea:Tarea,i:number){
+    this.listaDeTareas[i].estado = !tarea.estado;
+  }
 
 }
